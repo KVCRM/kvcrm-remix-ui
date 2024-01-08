d60aa7007926f51a5e15acd4424b24c8b35cdb36
Dima Denysenko <coding@dimdnk.com>
2024-01-08 19:29:21 +0200

    chore: enable react-admin dependency

--- app/root.tsx
+++ app/root.tsx
@@ -4,7 +4,8 @@ export default function App() {
   return (
     <html lang="en">
       <head>
-          <title>KVCRM</title>
+        <title>KVCRM</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"   />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
